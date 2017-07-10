52823ab677e4e37c38dfa65a6437a64aec12b055
Penny Labropoulou <penny@ilsp.athena-innovation.gr>
2017-07-10 13:30:05 +0300

    added mappings for licences from MS-OMTD list

--- omtd-share-annotations-util/src/main/java/eu/openminted/share/annotations/util/analyzer/LicenseMappings.java
+++ omtd-share-annotations-util/src/main/java/eu/openminted/share/annotations/util/analyzer/LicenseMappings.java
@@ -19,6 +19,34 @@ public final class LicenseMappings
         urlToSpdx.put("www.apache.org/licenses/LICENSE-2.0.txt", APACHE_2_0);
         urlToSpdx.put("www.gnu.org/licenses/gpl-3.0.en.html", GPL_3_0);
         urlToSpdx.put("www.gnu.org/licenses/gpl-3.0-standalone.html", GPL_3_0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by/4.0/", CC-BY-4.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-nc/4.0/", CC-BY-NC-4.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-nc-nd/4.0/", CC-BY-NC-ND-4.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-nc-sa/4.0/", CC-BY-NC-SA-4.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-nd/4.0/", CC-BY-ND-4.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-sa/4.0/", CC-BY-SA-4.0);
+        urlToSpdx.put("https://creativecommons.org/publicdomain/zero/1.0/", CC0-1.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by/3.0/", CC-BY-3.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-nc/3.0/", CC-BY-NC-3.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-nc-nd/3.0/", CC-BY-NC-ND-3.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-nc-sa/3.0/", CC-BY-NC-SA-3.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-nd/3.0/", CC-BY-ND-3.0);
+        urlToSpdx.put("https://creativecommons.org/licenses/by-sa/3.0/", CC-BY-SA-3.0);
+        urlToSpdx.put("https://opendatacommons.org/licenses/pddl/", PDDL);
+        urlToSpdx.put("https://opendatacommons.org/licenses/by/1-0/", ODC-BY-1.0);
+        urlToSpdx.put("https://opendatacommons.org/licenses/odbl/1-0/", ODbL-1.0);
+        urlToSpdx.put("http://www.affero.org/oagpl.html", AGPL-1.0);
+        urlToSpdx.put("http://directory.fsf.org/wiki/License:BSD_4Clause", BSD-4-Clause);
+        urlToSpdx.put("https://opensource.org/licenses/BSD-3-Clause", BSD-3-Clause);
+        urlToSpdx.put("https://opensource.org/licenses/BSD-2-Clause", BSD-2-Clause);
+        urlToSpdx.put("https://www.gnu.org/licenses/fdl-1.3.en.html", GFDL-1.3);
+        urlToSpdx.put("https://www.gnu.org/licenses/lgpl-3.0.en.html", LGPL-3.0);
+        urlToSpdx.put("https://opensource.org/licenses/MIT", MIT);
+        urlToSpdx.put("https://wordnet.princeton.edu/wordnet/license/", Princeton-WordNet);
+        urlToSpdx.put("https://www.eclipse.org/legal/epl-v10.html", EPL-1.0);
+        urlToSpdx.put("https://www.nlm.nih.gov/databases/license/license.pdf", NLM);
+        urlToSpdx.put("https://www.hbz-nrw.de/produkte/open-access/lizenzen/dppl/fdppl/f-DPPL_v3_en_11-2008", f-DPPL-3.0);
+        urlToSpdx.put("https://www.hbz-nrw.de/produkte/open-access/lizenzen/dppl/mdppl/m-DPPL_v3_en_11-2008", M-DPPL-3.0);
     }
     
     private LicenseMappings()
