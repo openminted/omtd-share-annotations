fcf61a15f1e21945314ee2e75bf4cea8a0b62f06
Richard Eckart de Castilho <richard.eckart@gmail.com>
2018-03-26 19:09:20 -0300

    No issue. Prepare for release
    - Fix JavaDoc

--- omtd-share-annotations-api/src/main/java/eu/openminted/share/annotations/api/Language.java
+++ omtd-share-annotations-api/src/main/java/eu/openminted/share/annotations/api/Language.java
@@ -23,11 +23,12 @@ import java.lang.annotation.Target;
 
 /**
  * Mapping of language from OMTD-SHARE schema http://www.meta-share.org/OMTD-SHARE_XMLSchema/v201/OMTD-SHARE-ResourceCommon.xsd
- * <br/><br/>
+ * <p>
  * The language that is used in the resource or supported by the component, as specified in the BCP47 guidelines
  * (https://tools.ietf.org/html/bcp47); the language tag is built upon (a) language tag according to ISO 639-1 and for
  * languages not covered by this, the ISO 639-3; (b) the script tag according to ISO 15924; (c) the region tag
  * according to ISO 3166-1; (d) the variant subtag
+ * </p>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({java.lang.annotation.ElementType.TYPE})
