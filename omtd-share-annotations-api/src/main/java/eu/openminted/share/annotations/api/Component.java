48cb715e76fb047048fd5221c95e240c205fc59e
Richard Eckart de Castilho <richard.eckart@gmail.com>
2018-03-28 21:55:21 -0300

    No issue. Formatting.

--- omtd-share-annotations-api/src/main/java/eu/openminted/share/annotations/api/Component.java
+++ omtd-share-annotations-api/src/main/java/eu/openminted/share/annotations/api/Component.java
@@ -21,19 +21,23 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import eu.openminted.share.annotations.api.constants.OperationType;
+
 /**
- * Marks a class as an OpenMinTeD-compatible TDM component.
- * A piece of software typically for a specific technical purpose, such as a particular implementation of a
- * part-of-speech tagger (e.g TreeTagger), a tree parsing program (e.g. mstparser), etc.; it is wrapped in a standard
- * way within a particular component-oriented framework such as UIMA, GATE, etc. or as a specific type of web service.
+ * Marks a class as an OpenMinTeD-compatible TDM component. A piece of software typically for a
+ * specific technical purpose, such as a particular implementation of a part-of-speech tagger (e.g
+ * TreeTagger), a tree parsing program (e.g. mstparser), etc.; it is wrapped in a standard way
+ * within a particular component-oriented framework such as UIMA, GATE, etc. or as a specific type
+ * of web service.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({java.lang.annotation.ElementType.TYPE})
+@Target({ java.lang.annotation.ElementType.TYPE })
 public @interface Component
 {
     /**
      * Component type from the OpenMinTeD-SHARE inventory that is associated with this component.
-     * @see eu.openminted.share.annotations.api.constants.ComponentConstants.ComponentTypeConstants ComponentType permitted values
+     * 
+     * @see OperationType permitted values
      */
     String value() default "";
 
@@ -41,6 +45,6 @@ public @interface Component
      * Indicates whether the component can be used as an integrated end-user application
      */
     boolean application() default false;
-    
+
     // String applicationFunction();
 }
